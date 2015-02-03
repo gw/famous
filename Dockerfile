11c0f1220bd081792c679fa051a4efee785fb64e
Grant Wu <grantwu2@gmail.com>
2015-02-03 14:04:18 -0800

    (feature) Complete Dockerization

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,14 @@ MAINTAINER Grant Wu
 
 RUN apt-get update
 
-RUN apt-get install -y python python-pip
+RUN apt-get install -y build-essential python-dev python-pip
 
 ADD . /src
 
-RUN pip install -r /src/requirements.txt
-
 WORKDIR /src
+
+RUN pip install -r /src/requirements.txt
 
 EXPOSE 8000
 
-CMD ["gunicorn", "app:app"]
+CMD ["gunicorn", "-b 0.0.0.0", "app:app"]
--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 from pymongo import MongoClient
-#client = MongoClient('mongodb://172.17.0.3:27017/')
-#client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://172.17.0.3:27017/')
+db = client['Texts']
 
 app = Flask(__name__)
 
 @app.route('/')
 def read_text():
-    return 'Hello World!'
+    try:
+        oldestText = db.texts.find().sort("_id", 1).limit(1)[0]
+        textBlob = oldestText['text']
+        db.texts.remove(oldestText)
+    except IndexError:
+        return 'We are all out of text blobs. Come back later.'
+    except KeyError:
+        return 'Sorry; bad data. Try again.'
+    except:
+        print sys.exc_info()[0]
+        return 'Something went horribly wrong. What did you do?'
+
+    return jsonify(text=textBlob)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True
+    )
